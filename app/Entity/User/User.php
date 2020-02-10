944255dbecc90dd0dfbe8ff5e5e9863593f40d4f
vadim <vadim.vikentiev@mail.ru>
2020-02-10 23:00:17 +0200

    change Auth, add service

---
+++ app/Entity/User/User.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity\User;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Str;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $remember_token
+ * @property string $status
+ * @property string $verify_token
+ */
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    public const STATUS_WAIT = 'wait';
+    public const STATUS_ACTIVE = 'active';
+
+    protected $fillable = [
+        'name', 'email', 'password', 'status', 'verify_token',
+    ];
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * @param string $name
+     * @param string $email
+     * @param string $password
+     * @return User
+     */
+    public static function register(string $name, string $email, string $password): self
+    {
+        return static::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password),
+            'verify_token' => Str::uuid(),
+            'status' => self::STATUS_WAIT,
+        ]);
+    }
+
+    /**
+     * @param $name
+     * @param $email
+     * @return User
+     */
+    public static function new($name, $email): self
+    {
+        return static::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt(Str::random()),
+            'status' => self::STATUS_ACTIVE,
+        ]);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isWait(): bool
+    {
+        return $this->status === self::STATUS_WAIT;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->status === self::STATUS_ACTIVE;
+    }
+
+    public function verify(): void
+    {
+        if (!$this->isWait()) {
+            throw new \DomainException('User is already verified.');
+        }
+
+        $this->update([
+            'status' => self::STATUS_ACTIVE,
+            'verify_token' => null,
+        ]);
+    }
+}
