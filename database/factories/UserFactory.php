4c78bab1177ff4b8ec7e5498262033c94cfde73c
vadim <vadim.vikentiev@mail.ru>
2020-02-11 22:47:59 +0200

    add UsersTableSeeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Entity\User\User;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +15,14 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
+    $active = $faker->boolean;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
+        'verify_token' => !$active ? Str::uuid() : null,
+        'status' => $active ? User::STATUS_ACTIVE : User::STATUS_WAIT
     ];
 });
